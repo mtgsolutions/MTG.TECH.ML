14d9f887f8c240d740df52345603759405e7c85d
Milton Santos <miltont.santos@outlook.com>
2024-10-06 22:08:29 -0300

    Add project files.

---
+++ PredictiveMaintenance.Api/Controllers/PredictiveMaintenanceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PredictiveMaintenance;
+using static PredictiveMaintenance.PredictiveMaintenanceModel;
+
+namespace PredictiveMaintenance.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredictiveMaintenanceController : ControllerBase
+    {
+
+
+        [HttpPost]
+        [Route("/Maintenance/Predict")]
+        public async Task<IActionResult> Predict([FromBody] ModelInput input)
+        {
+            var response = PredictiveMaintenanceModel.Predict(input);
+            return  Ok(response);
+        }
+    }
+}
